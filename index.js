8f941b97c280073c4a27b215d2a9c7bf26cdf9fa
Joey Trapp <joey@loadsys.com>
2013-07-23 08:57:23 -0700

    Added support for a before and after callback to be able to hook in custom middleware

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ module.exports = function(data, done) {
       staticFiles = data.staticFiles || false,
       routes = data.routes || false,
       proxy = data.proxy || false,
+      before = data.before || false,
+      after = data.after || false,
       app = express();
 
+  if (typeof before === 'function') { before.call(null, app); }
   if (staticFiles) { attach.staticFiles(app, staticFiles); }
   if (testConf) { testable.attach(app, testConf); }
   if (routes) { attach.routes(app, routes); }
   if (proxy) { attach.proxy(app, proxy); }
+  if (typeof after === 'function') { after.call(null, app); }
 
   app.listen(port, host, done);
 };
